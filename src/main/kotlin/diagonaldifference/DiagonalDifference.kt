87e52a4175e6ddcdd2dcecc9febc60630b06a97a
nameofthelaw <tsibin.andr@gmail.com>
2017-08-06 01:21:12 +0300

    feat: Add implementation for diagonal difference task

--- src/main/kotlin/diagonaldifference/DiagonalDifference.kt
+++ src/main/kotlin/diagonaldifference/DiagonalDifference.kt
@@ -6,11 +6,14 @@ fun main(args: Array<String>) {
     val scan = Scanner(System.`in`)
 
     val size = scan.nextInt()
-    val matrix = Array(size) { Array(size) {scan.nextInt()}}
+    val matrix = Array(size) { Array(size) { scan.nextInt() } }
 
     println(diagonalAbsoluteDifference(matrix))
 }
 
 fun diagonalAbsoluteDifference(matrix: Array<Array<Int>>): Int {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    return matrix
+            .foldIndexed(Pair(0, 0)) { i, acc, array ->
+                Pair(acc.first + array[i], acc.second + array[array.size - 1 - i]) }
+            .let { Math.abs(it.first - it.second) }
 }
