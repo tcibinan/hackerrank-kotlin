82a104d5fcd28ba218cb6e617027234b484c1bc2
nameofthelaw <tsibin.andr@gmail.com>
2017-08-08 21:35:33 +0300

    test: Add data driven tests for mini max sum task

--- src/main/kotlin/minimaxsum/MiniMaxSum.kt
+++ src/main/kotlin/minimaxsum/MiniMaxSum.kt
@@ -4,11 +4,11 @@ import java.util.*
 
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
-    val (a, b) = miniMaxSum(Array(5) { scanner.nextInt() })
+    val (a, b) = miniMaxSumFunctional(Array(5) { scanner.nextInt() })
     println("$a $b")
 }
 
-fun miniMaxSum(nums: Array<Int>): Pair<Long, Long> =
+fun miniMaxSumFunctional(nums: Array<Int>): Pair<Long, Long> =
         nums.sortedArray().foldIndexed(Pair(0L, 0L)) { index, acc, int ->
             if (index == 0) Pair(int.toLong(), 0L)
             else if (index == nums.size - 1) Pair(acc.first, acc.second + int)
--- src/test/kotlin/minimaxsum/MiniMaxSumKtTest.kt
+++ src/test/kotlin/minimaxsum/MiniMaxSumKtTest.kt
@@ -3,35 +3,33 @@ package minimaxsum
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
 
 import org.junit.Assert.*
 
-object MiniMaxSumKtTest : Spek({
-    describe("calculation min and max sums on numbers") {
-        val miniMaxSum = ::miniMaxSum
-        var nums: Array<Int>
-        var minMaxPair: Pair<Long, Long>
+class MiniMaxSumSpec : Spek({
+    describe("calculation min and max sums") {
 
-        on("five equals numbers") {
-            val number = 5
-            minMaxPair = miniMaxSum(Array(5) { number })
-            it("should return two sums equals to number*4") {
-                assertEquals(Pair(number * 4L, number * 4L), minMaxPair)
+        val miniMaxSum = ::miniMaxSumFunctional
+
+        on("array of five %s's",
+                data(0, Pair(0L, 0L)),
+                data(5, Pair(20L, 20L))
+        ) { num, expected ->
+            it("should return ${expected.first} as a mins and maxs sums") {
+                assertEquals(expected, miniMaxSum(Array(5) { num }))
             }
         }
 
-        on("five zeros") {
-            minMaxPair = miniMaxSum(Array(5) { 0 })
-            it("should return two zeros") {
-                assertEquals(Pair(0L, 0L), minMaxPair)
-            }
-        }
-
-        on("five different numbers") {
-            minMaxPair = minimaxsum.miniMaxSum(arrayOf(1, 2, 3, 4, 5))
-            it("should return sum of 4 minumum values and 4 maximum values") {
-                assertEquals(Pair(10L, 14L), minMaxPair)
+        on("array of different numbers: %s",
+                data(arrayOf(1, 2, 3, 4, 5), Pair(10L, 14L)),
+                data(arrayOf(5, 4, 3, 2, 1), Pair(10L, 14L)),
+                data(arrayOf(5, 0, 0, 0, 0), Pair(0L, 5L)),
+                data(arrayOf(5, 5, 5, 5, 10), Pair(20L, 25L))
+        ) { array, expected ->
+            it("should return ${expected.first} as a mins sum and ${expected.second} as a maxs sums") {
+                assertEquals(expected, miniMaxSum(array))
             }
         }
     }
